3123b6a97afeab9ab1d1d5223f513a4ad78c117b
kenshikento <tien-nguyen1@hotmail.co.uk>
2022-06-23 15:43:22 +0100

    testing if tag will be shown

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,7 +1,7 @@
 <template>
     <DefaultField :field="field" :errors="errors">
         <template slot="field">
-
+            <p>field is here</p>
             <div :class="{'flex flex-wrap' : !field.stack, 'border-danger': hasError}">
                 <div v-for="(option, val) in field.options" :class="{'mb-2' : field.stack || field.addPadding}"  class="mlbz-radio-container">
                     <label :for="`${field.attribute}_${val}`">
