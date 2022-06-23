49e6f724a5368037cd820e4949fd3e60d92984c6
kenshikento <tien-nguyen1@hotmail.co.uk>
2022-06-23 13:37:36 +0100

    tried adjusting webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,19 @@
-let mix = require('laravel-mix')
-let path = require('path')
-
-require('./nova.mix')
+let mix = require('laravel-mix');
+let path = require('path');
 
 mix
   .setPublicPath('dist')
   .js('resources/js/field.js', 'js')
-  .sass('resources/sass/field.scss', 'css')
   .vue({ version: 3 })
-  .nova('owenmelbz/nova-radio-field')
+  .webpackConfig({
+    externals: {
+      vue: 'Vue',
+    },
+    output: {
+      uniqueName: 'owenmelbz/nova-radio-field',
+    },
+  })
+  .alias({
+    'laravel-nova': path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/packages.js'),
+  })
+  .sass('resources/sass/field.scss', 'css');
